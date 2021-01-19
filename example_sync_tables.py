63150b18d46ff49f7687ed993210bac2502f8cd2
Hojin Jeong <tbop02@kaist.ac.kr>
2021-01-19 11:55:06 +0900

    add example_sync_tables.py

---
+++ example_sync_tables.py
@@ -0,0 +1,40 @@
+from x_x import account_info as ai
+
+class Infor:        
+    table_list = []    
+        
+    ## ople
+    table_list.append({
+        'acc_from' : ai.analdb_sql_info(),
+        'acc_to' : ai.vmdb_sql_info(),
+        'db_from': 'ople',
+        'table_from' : 'rest_product',
+        'db_to' : 'ople',
+        'table_to' : 'rest_product',
+        'primary_key' : 'id'
+    })
+    table_list.append({
+        'acc_from' : ai.analdb_sql_info(),
+        'acc_to' : ai.vmdb_sql_info(),
+        'db_from': 'ople',
+        'table_from' : 'rest_store',
+        'db_to' : 'ople',
+        'table_to' : 'rest_store',
+        'primary_key' : 'id'
+    })
+    
+
+class Infor_urgent:        
+    table_list = []    
+    
+    table_list.append({
+        'acc_from' : ai.analdb_sql_info(),
+        'acc_to' : ai.vmdb_sql_info(),
+        'db_from': 'data_sc',
+        'table_from' : 'date_holiday',
+        'db_to' : 'data_sc',
+        'table_to' : 'date_holiday',
+        'primary_key' : 'id'
+    })
+    
+
